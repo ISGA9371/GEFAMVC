de0c330f0dd8724b40ab092fa2f647858d08ee9f
MANUEL GUEVARA GARCIA <manuel.guevara.garcia.contractor@dev.bbva.com>
2018-03-26 15:09:08 -0600

    update queries

--- src/main/java/com/mx/bbva/config/JPAPersistenceConfig.java
+++ src/main/java/com/mx/bbva/config/JPAPersistenceConfig.java
@@ -36,7 +36,7 @@ public class JPAPersistenceConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://23.251.150.233:3306/gestion_factoria?serverTimezone=America/Mexico_City&" +
                 "useSSL=false&zeroDateTimeBehavior=convertToNull");
         dataSource.setUsername("gefaapp");
