c56114cbe3faa3b662c81d205ca7b01959868549
Ernesto <cars.9371@gmail.com>
2018-03-27 15:21:09 -0600

    Actualicacion para integrar las consultas de Tipologia v1.2

--- src/main/java/com/mx/bbva/business/repository/RequirementRepository.java
+++ src/main/java/com/mx/bbva/business/repository/RequirementRepository.java
@@ -4,7 +4,8 @@ import com.mx.bbva.business.entity.Requirement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface RequirementRepository extends JpaRepository<Requirement, Integer> {
-
+    Requirement findByRequirementId(Integer requirementId);
 }
