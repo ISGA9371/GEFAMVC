af8ed24bcade9af17d4142c8cef1c28ab68b3609
badiz <ricardo.bahena.contractor@bbva.com>
2018-03-31 16:09:58 -0600

    Cambios alta de PEP

--- src/main/java/com/mx/bbva/controller/BudgetController.java
+++ src/main/java/com/mx/bbva/controller/BudgetController.java
@@ -1,7 +1,7 @@
 package com.mx.bbva.controller;
 
-import com.mx.bbva.business.entity.Budget;
-import com.mx.bbva.business.service.BudgetService;
+import com.mx.bbva.business.entity.*;
+import com.mx.bbva.business.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -21,6 +22,10 @@ public class BudgetController {
     private static final Logger LOG = Logger.getLogger(BudgetController.class.getName());
 
     private BudgetService budgetService;
+    private AreaService areaService;
+    private LevelService levelService;
+    private LevelTypeService levelTypeService;
+    private UserService userService;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String createBudget(Model model) {
@@ -65,8 +70,63 @@ public class BudgetController {
         return URL_FACTORY + EDIT_BUDGET;
     }
 
+    @ModelAttribute("areas")
+    public List<Area> populateAreas() {
+        return this.areaService.findAllAreas();
+    }
+
+    //FALTA COMBO BANCAS TABLAS 34
+
+    //FALTA COMBO ENTIDAD TABLAS 37
+
+    // LevelTypeId 1 - Direccion
+    @ModelAttribute("principals")
+    public List<Level> populatePrincipals() {
+        return this.levelService.findByLevelType(new LevelType(1));
+    }
+
+    // LevelTypeId 2 - Sub-Direccion
+    @ModelAttribute("subPrincipals")
+    public List<Level> populateSubPrincipals() {
+        LOG.info(this.levelService.findByLevelType(new LevelType(2)).get(2).getLevelSuperior() + "");
+
+        return this.levelService.findByLevelType(new LevelType(2));
+    }
+
+    @ModelAttribute("users")
+    public List<User> populateUsers() {
+        List<User> users = new ArrayList<>();
+        // TODO Use Enum's
+        // 1 - Gestoria FSW
+        users.addAll(this.userService.findUsersByType(1));
+        // 2 - Gestoria PBAS
+        users.addAll(this.userService.findUsersByType(2));
+        return users;
+    }
+
+    // Import services
+    @Autowired
+    public void setAreaService(AreaService areaService) {
+        this.areaService = areaService;
+    }
+
+    @Autowired
+    public void setLevelService(LevelService levelService) {
+        this.levelService = levelService;
+    }
+
+    @Autowired
+    public void setLevelTypeService(LevelTypeService levelTypeService) {
+        this.levelTypeService = levelTypeService;
+    }
+
     @Autowired
     public void setBudgetService(BudgetService budgetService) {
         this.budgetService = budgetService;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 }
