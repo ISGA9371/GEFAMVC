46e6294ca6e1b82535c438bfd2f0d10bbb33fcc5
Isai Garcia <cars.9371@gmail.com>
2018-04-03 19:05:51 -0500

    high doubt with hard code

--- src/main/java/com/mx/bbva/controller/DoubtController.java
+++ src/main/java/com/mx/bbva/controller/DoubtController.java
@@ -1,5 +1,6 @@
 package com.mx.bbva.controller;
 
+import com.mx.bbva.business.dto.highdoubtDTO;
 import com.mx.bbva.business.entity.*;
 import com.mx.bbva.business.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,22 @@ public class DoubtController {
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String addDoubt(Model model, @RequestParam(value = "componentId") Integer componentId) {
-        Component component = componentService.findComponent(componentId);
-        model.addAttribute("componentData", component);
+        //Component component = componentService.findComponent(componentId);
+
+        highdoubtDTO hddto= new highdoubtDTO();
+
+
+            hddto.setComponentName("compnentes1");
+            hddto.setComponentVersion("1");
+            hddto.setLevelName("2");
+            hddto.setRequirementName("requerimiento1");
+            hddto.setTechnologyName("techonologi1");
+            hddto.setTypologyProductId("tipologia1");
+            hddto.setUserInternalId("xmz8142");
+
+
+        //model.addAttribute("componentData", component);
+        model.addAttribute("componentData", hddto);
         model.addAttribute("doubt", new Doubt());
         return URL_FACTORY + NEW_DOUBT;
     }
@@ -47,7 +62,7 @@ public class DoubtController {
 
     // Model Attributes will available to the view all the time
     // LevelTypeId 1 - Direccion
-    @ModelAttribute("principals")
+   /* @ModelAttribute("principals")
     public List<Level> populatePrincipals() {
         return this.levelService.findByLevelType(new LevelType(1));
     }
@@ -61,7 +76,7 @@ public class DoubtController {
     @ModelAttribute("companies")
     public List<Company> populateCompanies() {
         return this.companyService.findAllCompaniesForDropDownList();
-    }
+    }*/
 
     @ModelAttribute("priorities")
     public List<Priority> populatePriorities() {
