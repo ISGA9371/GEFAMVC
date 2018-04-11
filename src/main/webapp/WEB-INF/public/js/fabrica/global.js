88a8c98833791a08fb4173dc29e42d6698d3be41
Ernesto <cars.9371@gmail.com>
2018-04-11 18:09:23 -0500

    Update por cambio en los Combos.

--- src/main/webapp/WEB-INF/public/js/fabrica/global.js
+++ src/main/webapp/WEB-INF/public/js/fabrica/global.js
@@ -1,5 +1,25 @@
 function initGlobal(){
     $(".mdc-menu").width($(".mdc-select").filter(":visible").filter(":first").width());
+    scrollLockerOnSelec();
+}
+
+function scrollLockerOnSelec(){
+    $("html").on("click", function(event){
+        openedSelect = $(".mdc-select--open");
+        if($(event.target).attr("class") == "mdc-list-item"){
+            $("body").css({"overflow": "auto", "margin-right": "0px"});
+            $("footer").css({"width": "100%"});
+        } else {
+            if(openedSelect.length > 0 && (window.innerHeight < (document.body.clientHeight+40))){
+                $("body").css({"overflow": "hidden", "margin-right": getScrollBarWidth()+"px"});
+                $("footer").css({"width":"100%"});
+                $("footer").css({"width":+(100-(getScrollBarWidth()*100)/$("footer").width()).toFixed(2)+"%"});
+            } else {
+                $("body").css({"overflow": "auto", "margin-right": "0px"});
+                $("footer").css({"width": "100%"});
+            }
+        }
+    });
 }
 
 function customHolder(type, msg, fctn) {
@@ -29,6 +49,32 @@ function customHolder(type, msg, fctn) {
         textColor: "white"
     });
 }
+
+function getScrollBarWidth() {
+    var inner = document.createElement('p');
+    inner.style.width = "100%";
+    inner.style.height = "200px";
+
+    var outer = document.createElement('div');
+    outer.style.position = "absolute";
+    outer.style.top = "0px";
+    outer.style.left = "0px";
+    outer.style.visibility = "hidden";
+    outer.style.width = "200px";
+    outer.style.height = "150px";
+    outer.style.overflow = "hidden";
+    outer.appendChild (inner);
+
+    document.body.appendChild (outer);
+    var w1 = inner.offsetWidth;
+    outer.style.overflow = 'scroll';
+    var w2 = inner.offsetWidth;
+    if (w1 == w2) w2 = outer.clientWidth;
+
+    document.body.removeChild (outer);
+
+    return (w1 - w2);
+};
 
 var msgAssets = new Object();
 msgAssets.info = new Object();
