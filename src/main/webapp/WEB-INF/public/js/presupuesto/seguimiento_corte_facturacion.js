c4019179befa92cf15e4f1d41d1bf66a86f7bac1
Jonathan Leon <jonathan_leonl@hotmail.com>
2018-04-17 03:27:43 -0500

    search select fields

--- src/main/webapp/WEB-INF/public/js/presupuesto/seguimiento_corte_facturacion.js
+++ src/main/webapp/WEB-INF/public/js/presupuesto/seguimiento_corte_facturacion.js
@@ -26,6 +26,63 @@ $(document).ready(function(){
     return date;
   }
 
+  $.ajax({
+    url: "/api/level-types/1/levels",
+    method: "GET",
+    beforeSend: function () {
+      $("#mdc_select_direccion").addClass("mdc-select--disabled");
+      $("#mdc_select_direccion > .mdc-select__surface > .mdc-select__selected-text").html("");
+      $("#mdc_select_direccion > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
+    }
+  }).done(function (data) {
+    lis = "";
+    if ('undefined' !== typeof data.data && 0 < data.data.length) {
+      $.each(data.data, function (index, value) {
+        lis += "<li class='mdc-list-item' role='option' tabindex='0' id='" + value.levelId + "'>" + value.levelName + "</li>";
+      });
+
+      $("#mdc_select_direccion > .mdc-menu > ul").append(lis);
+      $("#mdc_select_direccion").removeClass("mdc-select--disabled");
+    } else {
+      lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
+      $("#mdc_select_direccion > .mdc-menu > ul").append(lis);
+      $("#mdc_select_direccion").addClass("mdc-select--disabled");
+    }
+  }).fail(function () {
+    lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
+    $("#mdc_select_direccion > .mdc-menu > ul").append(lis);
+    $("#mdc_select_direccion").addClass("mdc-select--disabled");
+  });
+
+
+  /** EDO PEP FACT */
+  $.ajax({
+    url: "/api/status-types/13/status",
+    method: "GET",
+    beforeSend: function () {
+      $("#mdc_select_estado_pep_factura").addClass("mdc-select--disabled");
+      $("#mdc_select_estado_pep_factura > .mdc-select__surface > .mdc-select__selected-text").html("");
+      $("#mdc_select_estado_pep_factura > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
+    }
+  }).done(function (data) {
+    lis = "";
+    if ('undefined' !== typeof data.data && 0 < data.data.length) {
+      $.each(data.data, function (index, value) {
+        lis += "<li class='mdc-list-item' role='option' tabindex='0' id='" + value.statusId + "'>" + value.statusName + "</li>";
+      });
+      $("#mdc_select_estado_pep_factura > .mdc-menu > ul").append(lis);
+      $("#mdc_select_estado_pep_factura").removeClass("mdc-select--disabled");
+    } else {
+      lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
+      $("#mdc_select_estado_pep_factura > .mdc-menu > ul").append(lis);
+      $("#mdc_select_estado_pep_factura").addClass("mdc-select--disabled");
+    }
+  }).fail(function () {
+    lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
+    $("#mdc_select_estado_pep_factura > .mdc-menu > ul").append(lis);
+    $("#mdc_select_estado_pep_factura").addClass("mdc-select--disabled");
+  });
+
 
   mdc_text_fecha_corte = new mdc.textField.MDCTextField(document.querySelector('#mdc_text_fecha_corte'));
   mdc_text_pep = new mdc.textField.MDCTextField(document.querySelector('#mdc_text_pep'));
@@ -65,6 +122,43 @@ $(document).ready(function(){
   });
 
 
+  mdc_select_direccion.listen('MDCSelect:change', () => {
+    idSuperior = mdc_select_direccion.value;
+
+    mdc_select_subdireccion.selectedIndex = -1;
+    mdc_select_subdireccion.value = "";
+
+    $.ajax({
+      url: "/api/levels/" + idSuperior + "/sub-levels",
+      method: "GET",
+      beforeSend: function () {
+        $("#mdc_select_subdireccion").addClass("mdc-select--disabled");
+        $("#mdc_select_subdireccion > .mdc-select__surface > .mdc-select__selected-text").html("");
+        $("#mdc_select_subdireccion > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
+      }
+    }).done(function (data) {
+      $("#mdc_select_subdireccion > .mdc-menu > ul").html("");
+      lis = "";
+      if ( 'undefined' !== typeof data.data && 0 < data.data.length) {
+        $.each(data.data, function (index, value) {
+          lis += "<li class='mdc-list-item' role='option' tabindex='0' id='" + value.levelId + "'>" + value.levelName + "</li>";
+        });
+
+        $("#mdc_select_subdireccion > .mdc-menu > ul").append(lis);
+        $("#mdc_select_subdireccion").removeClass("mdc-select--disabled");
+      } else {
+        lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
+        $("#mdc_select_subdireccion > .mdc-menu > ul").append(lis);
+        $("#mdc_select_subdireccion").addClass("mdc-select--disabled");
+      }
+    }).fail(function () {
+      lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
+      $("#mdc_select_subdireccion > .mdc-menu > ul").append(lis);
+      $("#mdc_select_subdireccion").addClass("mdc-select--disabled");
+    });
+  });
+
+
   $("#btn_clear").click(function(){
     mdc_text_fecha_corte.value = "";
     mdc_text_pep.value = "";
@@ -93,10 +187,23 @@ $(document).ready(function(){
 
   $("#btn_search").click(function(){
 
+    var params = {
+      principalId: mdc_text_fecha_corte.value,
+      subPrincipalId: mdc_text_pep.value,
+      companyId: mdc_text_requerimiento.value,
+      technologyId: mdc_select_direccion.value,
+      typologyNewComponent: mdc_select_subdireccion.value,
+      statusId: mdc_select_estado_pep_factura.value,
+      startProductId: mdc_select_estado_pago.value,
+      finalProductId: mdc_text_fecha_desde.value,
+      statusTypologyId: mdc_text_fecha_hasta.value,
+    };
+    console.log(params);
+
     /*$.ajax({
       url: "/budgets/payments/search",
       method: "GET",
-      data: $.param($("#searchForm").serialize()),
+      data: $.param( params ),
       dataType: "json",
       beforeSend: function (xhr) {
         HoldOn.open({
