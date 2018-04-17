c91cb48c973acc59080b6a400daac733f5cbf51f
Jonathan Leon <jonathan_leonl@hotmail.com>
2018-04-17 03:28:04 -0500

    select search field seguimiento pago

--- src/main/webapp/WEB-INF/public/js/presupuesto/seguimiento_pago.js
+++ src/main/webapp/WEB-INF/public/js/presupuesto/seguimiento_pago.js
@@ -27,12 +27,12 @@ $(document).ready(function(){
   }
 
   $.ajax({
-    url: "/technologies",
+    url: "/api/technologies",
     method: "GET",
     beforeSend: function () {
-      $("#mdc_group_tecnologia").addClass("mdc-select--disabled");
-      $("#mdc_group_tecnologia > .mdc-select__surface > .mdc-select__selected-text").html("");
-      $("#mdc_group_tecnologia > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
+      $("#mdc_select_tecnologia").addClass("mdc-select--disabled");
+      $("#mdc_select_tecnologia > .mdc-select__surface > .mdc-select__selected-text").html("");
+      $("#mdc_select_tecnologia > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
     }
   }).done(function (data) {
     lis = "";
@@ -41,27 +41,27 @@ $(document).ready(function(){
         lis += "<li class='mdc-list-item' role='option' tabindex='0' id='" + value.technologyId + "'>" + value.technologyName + "</li>";
       });
 
-      $("#mdc_group_tecnologia > .mdc-menu > ul").append(lis);
-      $("#mdc_group_tecnologia").removeClass("mdc-select--disabled");
+      $("#mdc_select_tecnologia > .mdc-menu > ul").append(lis);
+      $("#mdc_select_tecnologia").removeClass("mdc-select--disabled");
     } else {
       lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
-      $("#mdc_group_tecnologia > .mdc-menu > ul").append(lis);
-      $("#mdc_group_tecnologia").addClass("mdc-select--disabled");
+      $("#mdc_select_tecnologia > .mdc-menu > ul").append(lis);
+      $("#mdc_select_tecnologia").addClass("mdc-select--disabled");
     }
   }).fail(function () {
     lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
-    $("#mdc_group_tecnologia > .mdc-menu > ul").append(lis);
-    $("#mdc_group_tecnologia").addClass("mdc-select--disabled");
+    $("#mdc_select_tecnologia > .mdc-menu > ul").append(lis);
+    $("#mdc_select_tecnologia").addClass("mdc-select--disabled");
   });
 
 
   $.ajax({
-    url: "/companies",
+    url: "/api/companies",
     method: "GET",
     beforeSend: function () {
-      $("#mdc_group_empresa").addClass("mdc-select--disabled");
-      $("#mdc_group_empresa > .mdc-select__surface > .mdc-select__selected-text").html("");
-      $("#mdc_group_empresa > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
+      $("#mdc_select_empresa").addClass("mdc-select--disabled");
+      $("#mdc_select_empresa > .mdc-select__surface > .mdc-select__selected-text").html("");
+      $("#mdc_select_empresa > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
     }
   }).done(function (data) {
     lis = "";
@@ -70,78 +70,83 @@ $(document).ready(function(){
         lis += "<li class='mdc-list-item' role='option' tabindex='0' id='" + value.companyId + "'>" + value.companyName + "</li>";
       });
 
-      $("#mdc_group_empresa > .mdc-menu > ul").append(lis);
-      $("#mdc_group_empresa").removeClass("mdc-select--disabled");
+      $("#mdc_select_empresa > .mdc-menu > ul").append(lis);
+      $("#mdc_select_empresa").removeClass("mdc-select--disabled");
     } else {
       lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
-      $("#mdc_group_empresa > .mdc-menu > ul").append(lis);
-      $("#mdc_group_empresa").addClass("mdc-select--disabled");
+      $("#mdc_select_empresa > .mdc-menu > ul").append(lis);
+      $("#mdc_select_empresa").addClass("mdc-select--disabled");
     }
   }).fail(function () {
     lis = "<li class='mdc-list-item' role='option' tabindex='0'></li>";
-    $("#mdc_group_empresa > .mdc-menu > ul").append(lis);
-    $("#mdc_group_empresa").addClass("mdc-select--disabled");
+    $("#mdc_select_empresa > .mdc-menu > ul").append(lis);
+    $("#mdc_select_empresa").addClass("mdc-select--disabled");
   });
 
 
-  mdc_solicitud = new mdc.textField.MDCTextField(document.querySelector('#mdc_group_solicitud'));
-  mdc_aceptacion = new mdc.textField.MDCTextField(document.querySelector('#mdc_group_aceptacion'));
-  mdc_fecha_envio = new mdc.textField.MDCTextField(document.querySelector('#mdc_group_fecha_envio'));
-  mdc_hoja_entrada = new mdc.textField.MDCTextField(document.querySelector('#mdc_group_hoja_entrada'));
-  mdc_estado = new mdc.textField.MDCTextField(document.querySelector('#mdc_group_estado'));
-  mdc_fecha_desde = new mdc.textField.MDCTextField(document.querySelector('#mdc_group_fecha_desde'));
-  mdc_fecha_hasta = new mdc.textField.MDCTextField(document.querySelector('#mdc_group_fecha_hasta'));
-  mdc_empresa = new mdc.select.MDCSelect(document.querySelector('#mdc_group_empresa'));
-  mdc_tecnologia = new mdc.select.MDCSelect(document.querySelector('#mdc_group_tecnologia'));
-  mdc_area_atencion = new mdc.textField.MDCTextField(document.querySelector('#mdc_group_area_atencion'));
-  mdc_orden_compra = new mdc.select.MDCSelect(document.querySelector('#mdc_group_orden_compra'));
-  
+  mdc_text_solicitud = new mdc.textField.MDCTextField(document.querySelector('#mdc_text_solicitud'));
+  mdc_text_aceptacion = new mdc.textField.MDCTextField(document.querySelector('#mdc_text_aceptacion'));
+  mdc_select_orden_compra = new mdc.select.MDCSelect(document.querySelector('#mdc_select_orden_compra'));
+  mdc_text_fecha_envio = new mdc.textField.MDCTextField(document.querySelector('#mdc_text_fecha_envio'));
+  mdc_text_hoja_entrada = new mdc.textField.MDCTextField(document.querySelector('#mdc_text_hoja_entrada'));
+  mdc_select_estado = new mdc.select.MDCSelect(document.querySelector('#mdc_select_estado'));
+  mdc_text_fecha_desde = new mdc.textField.MDCTextField(document.querySelector('#mdc_text_fecha_desde'));
+  mdc_text_fecha_hasta = new mdc.textField.MDCTextField(document.querySelector('#mdc_text_fecha_hasta'));
+  mdc_select_empresa = new mdc.select.MDCSelect(document.querySelector('#mdc_select_empresa'));
+  mdc_select_tecnologia = new mdc.select.MDCSelect(document.querySelector('#mdc_select_tecnologia'));
+  mdc_text_area_atencion = new mdc.textField.MDCTextField(document.querySelector('#mdc_text_area_atencion'));
+
+
 
   $("#fecha_envio").change(function(){
-    if ( "" != mdc_fecha_envio.value ) {
-      $("#mdc_group_fecha_envio > label").addClass("mdc-text-field__label--float-above");
+    if ( "" != mdc_text_fecha_envio.value ) {
+      $("#mdc_text_fecha_envio > label").addClass("mdc-text-field__label--float-above");
     } else {
-      $("#mdc_group_fecha_envio > label").removeClass("mdc-text-field__label--float-above");
+      $("#mdc_text_fecha_envio > label").removeClass("mdc-text-field__label--float-above");
     }
   });
 
 
   $("#fecha_desde").change(function () {
-    if ( "" != mdc_fecha_desde.value ) {
-      $("#mdc_group_fecha_desde > label").addClass("mdc-text-field__label--float-above");
+    if ( "" != mdc_text_fecha_desde.value ) {
+      $("#mdc_text_fecha_desde > label").addClass("mdc-text-field__label--float-above");
     } else {
-      $("#mdc_group_fecha_desde > label").removeClass("mdc-text-field__label--float-above");
+      $("#mdc_text_fecha_desde > label").removeClass("mdc-text-field__label--float-above");
     }
   });
 
 
   $("#fecha_hasta").change(function () {
-    if ( "" != mdc_fecha_hasta.value ) {
-      $("#mdc_group_fecha_hasta > label").addClass("mdc-text-field__label--float-above");
+    if ( "" != mdc_text_fecha_hasta.value ) {
+      $("#mdc_text_fecha_hasta > label").addClass("mdc-text-field__label--float-above");
     } else {
-      $("#mdc_group_fecha_hasta > label").removeClass("mdc-text-field__label--float-above");
+      $("#mdc_text_fecha_hasta > label").removeClass("mdc-text-field__label--float-above");
     }
   });
 
 
   $("#btn_clear").click(function(){
-    mdc_solicitud.value = "";
-    mdc_aceptacion.value = "";
-    mdc_fecha_envio.value = "";
-    mdc_hoja_entrada.value = "";
-    mdc_estado.value = "";
-    mdc_fecha_desde.value = "";
-    mdc_fecha_hasta.value = "";
-    mdc_empresa.selectedIndex = -1;
-    mdc_empresa.value = "";
-    $("#mdc_group_empresa > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
-    mdc_tecnologia.selectedIndex = -1;
-    mdc_tecnologia.value = "";
-    $("#mdc_group_tecnologia > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
-    mdc_area_atencion.value = "";
-    mdc_orden_compra.selectedIndex = -1;
-    mdc_orden_compra.value = "";
-    $("#mdc_group_orden_compra > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above");
+    mdc_text_solicitud.value = "";
+    mdc_text_aceptacion.value = "";
+    mdc_select_orden_compra.value = "";
+    mdc_text_fecha_envio.value = "";
+    mdc_text_hoja_entrada.value = "";
+    mdc_select_estado.value = "";
+    mdc_text_fecha_desde.value = "";
+    mdc_text_fecha_hasta.value = "";
+    mdc_select_empresa.value = "";
+    mdc_select_tecnologia.value = "";
+    mdc_text_area_atencion.value = "";
+
+    mdc_select_orden_compra.selectedIndex = -1;
+    mdc_select_estado.selectedIndex = -1;
+    mdc_select_empresa.selectedIndex = -1;
+    mdc_select_tecnologia.selectedIndex = -1;
+
+    $("#mdc_select_orden_compra > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above"); 
+    $("#mdc_select_estado > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above"); 
+    $("#mdc_select_empresa > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above"); 
+    $("#mdc_select_tecnologia > .mdc-select__surface > .mdc-select__label").removeClass("mdc-select__label--float-above"); 
 
     $("#fecha_hasta").datepicker("option", "minDate", null);
     $("#fecha_desde").datepicker("option", "maxDate", null);
@@ -151,20 +156,20 @@ $(document).ready(function(){
   $("#btn_submit").click(function(){
 
     var params = {
-      solicitud: mdc_solicitud.value,
-      aceptacion: mdc_aceptacion.value,
-      fecha_envio: mdc_fecha_envio.value,
-      hoja_entrada: mdc_hoja_entrada.value,
-      estado: mdc_estado.value,
-      fecha_desde: mdc_fecha_desde.value,
-      fecha_hasta: mdc_fecha_hasta.value,
-      empresa: mdc_empresa.value,
-      tecnologia: mdc_tecnologia.value,
-      area_atencion: mdc_area_atencion.value,
-      orden_compra: mdc_orden_compra.value,
+      solicitud: mdc_text_solicitud.value,
+      aceptacion: mdc_text_aceptacion.value,
+      fecha_envio: mdc_select_orden_compra.value,
+      hoja_entrada: mdc_text_fecha_envio.value,
+      estado: mdc_text_hoja_entrada.value,
+      fecha_desde: mdc_select_estado.value,
+      fecha_hasta: mdc_text_fecha_desde.value,
+      empresa: mdc_text_fecha_hasta.value,
+      tecnologia: mdc_select_empresa.value,
+      area_atencion: mdc_select_tecnologia.value,
+      orden_compra: mdc_text_area_atencion.value,
     };
 
-    console.log( $.param(params) );
+    console.log( params );
 
     // $.ajax({
     //   url: "/components/search",
