8e9ce241c345962d34e45e97ae3e73ff05babf44
cars.9371@gmail.com <Naya9371>
2018-05-09 11:29:12 -0500

    Window Combos

---
+++ src/main/java/com/mx/bbva/controller/ComboController.java
@@ -0,0 +1,36 @@
+package com.mx.bbva.controller;
+
+import com.mx.bbva.business.entity.*;
+import com.mx.bbva.business.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static com.mx.bbva.util.ViewsURLs.*;
+
+@Controller
+@RequestMapping(value = "/comobo")
+public class ComboController {
+
+    private static final Logger LOG = Logger.getLogger(IssueController.class.getName());
+
+    private IssueService issueService;
+    private OriginService originService;
+    private PriorityService priorityService;
+    private ComponentService componentService;
+    private UserService userService;
+    private LevelService levelService;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String createIssue(Model model, @RequestParam(value = "componentId") Integer componentId) {
+        // TODO Validate user
+        //TODO Add catalogs
+        return URL_ADMIN + NEW_COMBO;
+    }
+
+}
