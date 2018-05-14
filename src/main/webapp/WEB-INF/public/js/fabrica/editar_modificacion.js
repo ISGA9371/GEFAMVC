0b836336f0e92eee656e1adc20d1164e85810dfa
Jonathan Leon <jonathan_leonl@hotmail.com>
2018-05-13 20:11:24 -0500

    modifications

--- src/main/webapp/WEB-INF/public/js/fabrica/editar_modificacion.js
+++ src/main/webapp/WEB-INF/public/js/fabrica/editar_modificacion.js
@@ -1,3 +1,52 @@
+  var mdc_text_empresa = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_empresa"));
+  var mdc_text_requerimiento = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_requerimiento"));
+  var mdc_text_version = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_version"));
+  var mdc_text_componente = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_componente"));
+  var mdc_text_tecnologia = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_tecnologia"));
+  var mdc_text_fecha_envio = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_fecha_envio"));
+  var mdc_text_tipologia = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_tipologia"));
+  var mdc_text_dificultad = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_dificultad"));
+  var mdc_text_estado = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_estado"));
+  var mdc_text_tipologia_modificacion = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_tipologia_modificacion"));
+  var mdc_text_descripcion_modificacion = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_descripcion_modificacion"));
+  var mdc_select_prioridad = new mdc.select.MDCSelect(document.querySelector("#mdc_select_prioridad"));
+  var mdc_text_usuario_peticionario = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_usuario_peticionario"));
+  var mdc_select_origen = new mdc.select.MDCSelect(document.querySelector("#mdc_select_origen"));
+  var mdc_text_fecha_prevista_finalizacion = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_fecha_prevista_finalizacion"));
+  var mdc_text_fecha_negociada = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_fecha_negociada"));
+  var mdc_text_modificacion_vuelo = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_modificacion_vuelo"));
+  var mdc_select_estado = new mdc.select.MDCSelect(document.querySelector("#mdc_select_estado"));
+  var mdc_text_responsable_resolucion = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_responsable_resolucion"));
+  var mdc_text_fecha_cierre = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_fecha_cierre"));
+  var mdc_select_modifiacion_realizada_dyd = new mdc.select.MDCSelect(document.querySelector("#mdc_select_modifiacion_realizada_dyd"));
+  var mdc_text_comentarios = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_comentarios"));
+  var mdc_select_dificultad_modificacion_inicial = new mdc.select.MDCSelect(document.querySelector("#mdc_select_dificultad_modificacion_inicial"));
+  var mdc_text_horas_modificacion_inicial = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_horas_modificacion_inicial"));
+  var mdc_text_costo_modificacion_inicial = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_costo_modificacion_inicial"));
+  var mdc_select_dificultad_modificacion_final = new mdc.select.MDCSelect(document.querySelector("#mdc_select_dificultad_modificacion_final"));
+  var mdc_text_horas_modificacion_final = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_horas_modificacion_final"));
+  var mdc_text_costo_modificacion_final = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_costo_modificacion_final"));
+  var mdc_text_comentario_tipificacion_proveedor = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_comentario_tipificacion_proveedor"));
+  var mdc_text_comentario_tipificacion_proyecto = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_comentario_tipificacion_proyecto"));
+  var mdc_text_estado_tipificacion_final = new mdc.textField.MDCTextField(document.querySelector("#mdc_text_estado_tipificacion_final"));
+  var mdc_select_facturar = new mdc.select.MDCSelect(document.querySelector("#mdc_select_facturar"));
+
+
+$(document).ready(function () {
+
+
+
+  mdc_select_facturar.listen('MDCSelect:change', () => {
+    console.log(mdc_select_facturar.value);
+    if ( "1" == mdc_select_facturar.value ) {
+      $("#asociar-pep").show();
+    } else {
+      $("#asociar-pep").hide();
+    }
+  });
+
+});
+
 
 
 function init() {
