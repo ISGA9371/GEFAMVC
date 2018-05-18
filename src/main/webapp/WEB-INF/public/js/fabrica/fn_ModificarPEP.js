5ab5cec2474fa8730c45ab89bb159fa1875d3ef4
rodrigo <cars.9371@gmail.com>
2018-05-18 18:13:44 -0500

    Combo Selector Update

--- src/main/webapp/WEB-INF/public/js/fabrica/fn_ModificarPEP.js
+++ src/main/webapp/WEB-INF/public/js/fabrica/fn_ModificarPEP.js
@@ -10,7 +10,7 @@ function init(){
     //add default values
     addMissing();
     camp();
-
+    loadCombos()
 }
 
 function crearCombos(){
@@ -73,3 +73,153 @@ function camp() {
     new mdc.textField.MDCTextField(document.getElementById('pepIdDiv-js-text')).disabled = true;
 }
 
+function loadCombos(){
+
+    $.ajax({
+        async: false,
+        url: "/api/areas"
+    }).done(function (json) {
+        $.each(json.data, function (i, data) {
+            $liElement = $("<li>");
+            $liElement.attr("class", "mdc-list-item");
+            $liElement.attr("role", "option");
+            $liElement.attr("id", data.areaId);
+            $liElement.append(data.areaName);
+            $("#area-js-select").find("ul:first").append($liElement);
+        });
+        mdc.select.MDCSelect.attachTo(document.getElementById('area-js-select'));
+    }).fail(function (xhr, status, error) {
+        console.log('¡Error al consultar combos!');
+        new mdc.select.MDCSelect(document.getElementById('area-js-select')).disabled = true;
+        customHolder('error', '¡Error al consultar combos!');
+        showingError = true;
+        $('#hidden-area').val("");
+    });
+
+    $.ajax({
+        async: false,
+        url: "/api/bankings"
+    }).done(function (json) {
+        $.each(json.data, function (i, data) {
+            $liElement = $("<li>");
+            $liElement.attr("class", "mdc-list-item");
+            $liElement.attr("role", "option");
+            $liElement.attr("id", data.bankingId);
+            $liElement.append(data.bankingName);
+            $("#banca-js-select").find("ul:first").append($liElement);
+        });
+        mdc.select.MDCSelect.attachTo(document.getElementById('banca-js-select'));
+    }).fail(function (xhr, status, error) {
+        console.log('¡Error al consultar combos!');
+        new mdc.select.MDCSelect(document.getElementById('banca-js-select')).disabled = true;
+        customHolder('error', '¡Error al consultar combos!');
+        showingError = true;
+        $('#hidden-banca').val("");
+    });
+
+    $.ajax({
+        async: false,
+        url: "/api/corporations"
+    }).done(function (json) {
+        $.each(json.data, function (i, data) {
+            $liElement = $("<li>");
+            $liElement.attr("class", "mdc-list-item");
+            $liElement.attr("role", "option");
+            $liElement.attr("id", data.corporationId);
+            $liElement.append(data.corporationName);
+            $("#estado-js-select").find("ul:first").append($liElement);
+        });
+        mdc.select.MDCSelect.attachTo(document.getElementById('estado-js-select'));
+    }).fail(function (xhr, status, error) {
+        console.log('¡Error al consultar combos!');
+        new mdc.select.MDCSelect(document.getElementById('estado-js-select')).disabled = true;
+        customHolder('error', '¡Error al consultar combos!');
+        showingError = true;
+        $('#hidden-estado').val("");
+    });
+
+    $.ajax({
+        async: false,
+        url: "/api/natures"
+    }).done(function (json) {
+        $.each(json.data, function (i, data) {
+            $liElement = $("<li>");
+            $liElement.attr("class", "mdc-list-item");
+            $liElement.attr("role", "option");
+            $liElement.attr("id", data.natureId);
+            $liElement.append(data.natureName);
+            $("#ig-js-select").find("ul:first").append($liElement);
+        });
+        mdc.select.MDCSelect.attachTo(document.getElementById('ig-js-select'));
+    }).fail(function (xhr, status, error) {
+        console.log('¡Error al consultar combos!');
+        new mdc.select.MDCSelect(document.getElementById('ig-js-select')).disabled = true;
+        customHolder('error', '¡Error al consultar combos!');
+        showingError = true;
+        $('#hidden-ig').val("");
+    });
+
+    $.ajax({
+        async: false,
+        url: "/api/budgets-responsible"
+    }).done(function (json) {
+        $.each(json.data, function (i, data) {
+            $liElement = $("<li>");
+            $liElement.attr("class", "mdc-list-item");
+            $liElement.attr("role", "option");
+            $liElement.attr("id", data.budgetsResponsibleId);
+            $liElement.append(data.budgetsResponsibleName);
+            $("#responsableDot-js-select").find("ul:first").append($liElement);
+        });
+        mdc.select.MDCSelect.attachTo(document.getElementById('responsableDot-js-select'));
+    }).fail(function (xhr, status, error) {
+        console.log('¡Error al consultar combos!');
+        new mdc.select.MDCSelect(document.getElementById('responsableDot-js-select')).disabled = true;
+        customHolder('error', '¡Error al consultar combos!');
+        showingError = true;
+        $('#hidden-responsableDot').val("");
+    });
+
+    $.ajax({
+        async: false,
+        url: "/api/budgets-applicants"
+    }).done(function (json) {
+        $.each(json.data, function (i, data) {
+            $liElement = $("<li>");
+            $liElement.attr("class", "mdc-list-item");
+            $liElement.attr("role", "option");
+            $liElement.attr("id", data.budgetsApplicantId);
+            $liElement.append(data.budgetsApplicantName);
+            $("#solicitante-js-select").find("ul:first").append($liElement);
+        });
+        mdc.select.MDCSelect.attachTo(document.getElementById('solicitante-js-select'));
+    }).fail(function (xhr, status, error) {
+        console.log('¡Error al consultar combos!');
+        new mdc.select.MDCSelect(document.getElementById('solicitante-js-select')).disabled = true;
+        customHolder('error', '¡Error al consultar combos!');
+        showingError = true;
+        $('#hidden-solicitante').val("");
+    });
+
+    selectByValue(mdcSelectArea,"area-js-select",$("#hidden-area").val());
+    selectByValue(mdcSelectBanking,"banca-js-select",$("#hidden-banca").val());
+    selectByValue(mdcSelectEstado,"estado-js-select",$("#hidden-estado").val());
+    selectByValue(mdcSelectNature,"ig-js-select",$("#hidden-ig").val());
+    selectByValue(mdcSelectresponsableDot,"responsableDot-js-select",$("#hidden-responsableDot").val());
+    selectByValue(mdcSelectsolicitante,"solicitante-js-select",$("#hidden-solicitante").val());
+
+}
+
+function selectByValue(mdcSelect, mdcSelectId,  value){
+    $option = $("#"+mdcSelectId).parent().find("li[id='" + value + "']");
+    mdcSelect.item($option.parent().find("li").index($option)).click();
+    $(mdcSelect.label_).addClass("mdc-select__label--float-above");
+}
+
+
+mdcSelectArea = new mdc.select.MDCSelect($('#area-js-select')[0]);
+mdcSelectBanking = new mdc.select.MDCSelect($('#banca-js-select')[0]);
+mdcSelectEstado = new mdc.select.MDCSelect($('#estado-js-select')[0]);
+mdcSelectNature = new mdc.select.MDCSelect($('#ig-js-select')[0]);
+mdcSelectresponsableDot = new mdc.select.MDCSelect($('#responsableDot-js-select')[0]);
+mdcSelectsolicitante = new mdc.select.MDCSelect($('#solicitante-js-select')[0]);
