a13a3b3578b43017b157a3efda28b935221fea8f
Equipo 210 <omarrb9508@gmail.com>
2018-05-18 16:56:22 -0500

    Modifications above

--- src/main/webapp/WEB-INF/public/js/fabrica/fn_AltaModificar.js
+++ src/main/webapp/WEB-INF/public/js/fabrica/fn_AltaModificar.js
@@ -234,7 +234,8 @@ function ajaxGuardar() {
         customHolder("info", "Modificacion Registrada Exitosamente.","window.location.href = '/components/filters'; holder('Cargando...')");
         //customHolder("info", "Componente Dado de Alta Exitosamente.","$('html').html(okData);");
     }).fail(function (xhr, status, error) {
-        customHolder("error", xhr.responseJSON.message)
+            xhr.responseJSON = undefined;
+            customHolder("error", xhr.responseJSON.message);
     });
 }
 
